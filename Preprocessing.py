08765c66e8af6270082603a2fadcae4a10fbe1d0
Silleke <SCF.vanSambeek@student.han.nl>
2018-06-13 21:14:10 +0200

    Added the functionality of Define_paths_create_folders

--- Preprocessing.py
+++ Preprocessing.py
@@ -6,7 +6,22 @@ import numpy as np
 
 
 def main():
-    image_origin = r"C:\Users\sille\Pictures\ChinaSet_AllFiles\ChinaSet_AllFiles\CXR_png"
+    # This path is command line input from the user. This is the root for the filetree of the processed images
+    root_path_tuberculose_images_divided = r"/home/silvie/Pictures/Tuberculose_images_divided"
+
+    # This path is command line input from the user. This is where the downloaded raw images should be stored
+    image_origin = r"/home/silvie/Pictures/ChinaSet_AllFiles/CXR_png"
+
+    # Define the paths for training, validating and testing
+    train_paths = define_paths(path_base=root_path_tuberculose_images_divided, step="Train")
+    valid_paths = define_paths(path_base=root_path_tuberculose_images_divided, step="Valid")
+    test_paths = define_paths(path_base=root_path_tuberculose_images_divided, step="Test")
+
+    # Create the folders for storing the training, validating and testing images
+    create_folders(train_paths)
+    create_folders(valid_paths)
+    create_folders(test_paths)
+
     sick_healthy_dict = get_image_titles(image_origin)
 
     # Get the names of the images and convert the dict.keys object to a list
@@ -30,17 +45,40 @@ def main():
     validating_labels_list = create_labels_list(validation_data, sick_healthy_dict)
     testing_labels_list = create_labels_list(testing_data, sick_healthy_dict)
 
-    training_path = r"C:\Users\sille\Pictures\Tuberculose_images_divided\Train"
-    validating_path = r"C:\Users\sille\Pictures\Tuberculose_images_divided\Valid"
-    testing_path = r"C:\Users\sille\Pictures\Tuberculose_images_divided\Test"
-
-    write_images_to_folder(training_data, sick_healthy_dict, training_path, image_origin)
+    write_images_to_folder(training_data, sick_healthy_dict, train_paths, image_origin)
     print("Done training images")
-    write_images_to_folder(validation_data, sick_healthy_dict, validating_path, image_origin)
+    write_images_to_folder(validation_data, sick_healthy_dict, valid_paths, image_origin)
     print("Done validating images")
-    write_images_to_folder(testing_data, sick_healthy_dict, testing_path, image_origin)
+    write_images_to_folder(testing_data, sick_healthy_dict, test_paths, image_origin)
     print("Done testing images")
 
+
+def define_paths(path_base, step):
+    step_path = os.path.join(path_base, step)
+    base_path_positive = os.path.join(step_path, "Positive_TB")
+    base_path_negative = os.path.join(step_path, "Negative_TB")
+
+    path_list = [step_path, base_path_negative, base_path_positive]
+    return path_list
+
+
+# Creates all the needed folders at the path in path_list
+# If a folder already exists, all files from this folder are removed. Subdirectories won't be removed
+# path_list = a list with all the paths for the folders
+def create_folders(path_list):
+    for path in path_list:
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+            print("Created ", path)
+
+        else:
+            file_list = os.listdir(path)
+            for filename in file_list:
+                filepath = os.path.join(path, filename)
+                if os.path.isfile(filepath):
+                    os.remove(filepath)
+            print("Folder already existed. Removed all files from folder. ")
 
 # This function retrieves a list of file and directory names from the current working directory
 # This function filters the filenames out
@@ -61,11 +99,13 @@ def get_image_titles(image_origin):
                 values.append(1)
     return dict(zip(keys, values))
 
+
 # Calculate how many instances are used for training and validating
 # dataset is the complete dataset with images
 # instances_set_size is the size (as a fraction) of the training or validating set
 def calculate_number_of_instances(dataset, instances_set_size):
     return int(round((len(dataset) * instances_set_size), 0))
+
 
 # Get random instances from dataset for training and validating
 # dataset is the complete dataset with images
@@ -80,6 +120,7 @@ def get_random_instances(dataset, number_of_instances):
         list_data.append(placeholder)
     return list_data, dataset
 
+
 # Create a NumPy array with the labels of the images (0 means healthy, 1 means sick)
 # sick_healthy_dict is a dictionary with the healthy / sick status of each image
 def create_labels_list(datalist, sick_healthy_dict):
@@ -89,27 +130,28 @@ def create_labels_list(datalist, sick_healthy_dict):
         labels_list.append(placeholder)
     return np.array(labels_list)
 
+
 # Write the images to their respective folders
 # samples_list is a list with the respective samples
 # sick_healthy_dict is a dictionary with the healthy / sick status of each image
 # image_origin is the folder where the complete dataset can be found
-def write_images_to_folder(samples_list, sick_healthy_dict, path, image_origin):
+def write_images_to_folder(samples_list, sick_healthy_dict, path_list, image_origin):
     for image_title in samples_list:
         class_id = sick_healthy_dict.get(image_title)
-        image_origin_extended = image_origin + "/" + image_title
+        #image_origin_extended = image_origin + "/" + image_title
+        image_origin_extended = os.path.join(image_origin, image_title)
 
         if class_id == 0:
-            path_extended = path + "/Negative_TB"
             img = cv2.imread(image_origin_extended, 0)
-            cv2.imwrite(os.path.join(path_extended, image_title), img)
+            # cv2.imwrite(os.path.join(path_extended, image_title), img)
+            #path_negative = os.path.join(path_list, "Negative_TB")
+            cv2.imwrite(os.path.join(path_list[1], image_title), img)
 
 
         else:
-            path_extended = path + "/Positive_TB"
             img = cv2.imread(image_origin_extended, 0)
-            cv2.imwrite(os.path.join(path_extended, image_title), img)
-
-
+            #path_positive = os.path.join(path_list, "Positive_TB")
+            cv2.imwrite(os.path.join(path_list[2], image_title), img)
 
 
 main()
